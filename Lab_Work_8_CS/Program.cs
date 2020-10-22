56d63daa9c434f0573c9db339c8dbc258f0b013b
ArtyomKolosov2 <ArtyomKolosov2@users.noreply.github.com>
2020-10-22 19:02:43 +0300

    lab

---
+++ Lab_Work_8_CS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_Work_8_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
